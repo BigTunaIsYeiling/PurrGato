8f9a094a9ea3152ccc238cd008c95fade5841e48
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-10-13 03:56:20 +0300

    lkja

--- app/user/layout.js
+++ app/user/layout.js
@@ -36,29 +36,34 @@ const UserLayout = ({ children }) => {
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        overflow: "hidden", // Prevents extra scroll
       }}
     >
       <AppBar
-        position="static"
+        position="fixed"
         sx={{
-          backgroundColor: "transparent",
+          top: 0,
+          backgroundColor: "rgba(255, 255, 255, 0.1)",
           boxShadow: "none",
           display: "flex",
           alignItems: "center",
           mt: 2,
+          backdropFilter: "blur(10px)",
+          borderRadius: "50px",
+          padding: 2,
+          width: { xs: "96%", sm: "60%" },
+          maxWidth: "1200px", // Set a maximum width to prevent resizing
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
+          left: "50%",
+          transform: "translateX(-50%)", // Center it horizontally
         }}
       >
-        <Toolbar
+        <Box
           sx={{
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
-            backdropFilter: "blur(10px)",
-            borderRadius: "50px",
-            padding: 2,
-            width: { xs: "96%", sm: "60%" },
-            justifyContent: { xs: "center", sm: "space-between" },
-            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-            border: "1px solid rgba(255, 255, 255, 0.3)",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between", // Ensures proper alignment
           }}
         >
           <Box
@@ -70,7 +75,13 @@ const UserLayout = ({ children }) => {
           >
             <Image src={Logo} alt="PurrGatto Logo" width={40} height={40} />
           </Box>
-          <Box sx={{ display: "flex", gap: 3 }}>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              alignItems: "center",
+              gap: 3,
+            }}
+          >
             <Link href="/user" passHref>
               <IconButton
                 sx={{
@@ -123,11 +134,11 @@ const UserLayout = ({ children }) => {
             sx={{
               color: "#777",
               transition: "color 0.3s",
-              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Box shadow added
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
               "&:hover": {
-                color: "#FCE3CD", // Optional: Change color on hover
+                color: "#FCE3CD",
               },
-              ml: { xs: 3, sm: 0 },
+              display: { xs: "none", sm: "inline-flex " },
             }}
           >
             <Stack
@@ -144,23 +155,103 @@ const UserLayout = ({ children }) => {
               </Typography>
             </Stack>
           </IconButton>
+          <Box
+            sx={{
+              display: { xs: "flex", sm: "none" },
+              alignItems: "center",
+              gap: 3,
+              justifyContent: "space-between",
+              width: "100%",
+            }}
+          >
+            <Link href="/user" passHref>
+              <IconButton
+                sx={{
+                  color: pathname === "/user" ? "#FFBA6D" : "#777",
+                  transition: "color 0.3s",
+                }}
+              >
+                <VscHome size={24} />
+              </IconButton>
+            </Link>
+            <Link href={`/user/1`} passHref>
+              <IconButton
+                sx={{
+                  color:
+                    pathname == `/user` ||
+                    pathname == "/user/messages" ||
+                    pathname == "/user/notifications"
+                      ? "#777"
+                      : "#FFBA6D",
+                  transition: "color 0.3s",
+                }}
+              >
+                <VscAccount size={24} />
+              </IconButton>
+            </Link>
+            <Link href="/user/messages" passHref>
+              <IconButton
+                sx={{
+                  color: pathname === "/user/messages" ? "#FFBA6D" : "#777",
+                  transition: "color 0.3s",
+                }}
+              >
+                <VscMail size={24} />
+              </IconButton>
+            </Link>
+            <Link href="/user/notifications" passHref>
+              <IconButton
+                sx={{
+                  color:
+                    pathname === "/user/notifications" ? "#FFBA6D" : "#777",
+                  transition: "color 0.3s",
+                }}
+              >
+                <VscBell size={24} />
+              </IconButton>
+            </Link>
+            <IconButton
+              onClick={handleMenuClick}
+              sx={{
+                color: "#777",
+                transition: "color 0.3s",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                "&:hover": {
+                  color: "#FCE3CD",
+                },
+              }}
+            >
+              <Stack
+                sx={{
+                  width: "30px",
+                  height: "30px",
+                  borderRadius: "50%",
+                }}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Typography variant="body3" color="black">
+                  A
+                </Typography>
+              </Stack>
+            </IconButton>
+          </Box>
           <Menu
             anchorEl={anchorEl}
             open={Boolean(anchorEl)}
             onClose={handleMenuClose}
             PaperProps={{
               sx: {
-                backgroundColor: "rgba(255, 255, 255, 0.25)", // Semi-transparent background
-                backdropFilter: "blur(10px)", // Frosted glass effect
+                backgroundColor: "rgba(255, 255, 255, 0.25)",
+                backdropFilter: "blur(10px)",
                 borderRadius: 2,
                 border: "1px solid rgba(255, 255, 255, 0.18)",
-                boxShadow: "0 8px 32px rgba(0, 0, 0, 0.2)", // Glass-like shadow
+                boxShadow: "0 8px 32px rgba(0, 0, 0, 0.2)",
                 p: 1,
-                width: 250, // Adjust width to match
+                width: 250,
               },
             }}
           >
-            {/* Profile Information */}
             <UserDialog />
             <Divider />
             <MenuItem onClick={handleMenuClose}>
@@ -169,14 +260,16 @@ const UserLayout = ({ children }) => {
               </Typography>
             </MenuItem>
           </Menu>
-        </Toolbar>
+        </Box>
       </AppBar>
+      {/* Added padding-top equal to AppBar height to prevent content overlap */}
       <Box
         sx={{
           flexGrow: 1,
           padding: 2,
-          overflowY: "auto", // Ensure scrolling if content overflows
-          WebkitOverflowScrolling: "touch", // For smooth scrolling on iOS devices
+          paddingTop: "80px", // Adjust this height to match the AppBar height + margin
+          overflowY: "auto",
+          WebkitOverflowScrolling: "touch",
         }}
       >
         {children}
