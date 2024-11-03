9928b974eb4267b36dcbeccf647124463211c6d4
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-03 13:53:44 +0200

    btr

--- components/user/Answer.js
+++ components/user/Answer.js
@@ -8,6 +8,8 @@ import {
   Avatar,
   Typography,
   ListItemIcon,
+  Stack,
+  Button,
 } from "@mui/material";
 import { format, formatDistanceToNow } from "date-fns";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
@@ -18,6 +20,21 @@ import toast from "react-hot-toast";
 import ReAsk from "./ReAsk";
 import { IoShareOutline } from "react-icons/io5";
 import { AiOutlineDelete } from "react-icons/ai";
+import styled from "@emotion/styled";
+const GlassButton = styled(Button)({
+  background: "rgba(255, 255, 255, 0.25)",
+  backdropFilter: "blur(10px)",
+  border: "1px solid rgba(255, 255, 255, 0.3)",
+  borderRadius: "10px",
+  color: "black",
+  padding: "5px 10px",
+  textTransform: "none",
+  fontSize: "14px",
+  boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+  "&:hover": {
+    background: "rgba(255, 255, 255, 0.3)",
+  },
+});
 export const Answer = ({ post, avatar, username, userid, useridPosts }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   // Open delete menu
@@ -89,77 +106,33 @@ export const Answer = ({ post, avatar, username, userid, useridPosts }) => {
           position: "relative", // To position delete dots on top-right
         }}
       >
-        {useridPosts == userid && (
-          <>
-            <IconButton
-              aria-label="delete"
-              onClick={handleMenuClick}
-              sx={{
-                position: "absolute",
-                top: "10px",
-                right: "10px",
-                color: "#6A6A6A", // Neutral color for the dots
-              }}
-            >
-              <PiDotsThreeOutlineLight />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleMenuClose}
-              PaperProps={{
-                sx: {
-                  backgroundColor: "rgba(255, 255, 255, 0.25)", // Semi-transparent background
-                  backdropFilter: "blur(10px)", // Frosted glass effect
-                  borderRadius: 2,
-                  border: "1px solid rgba(255, 255, 255, 0.18)",
-                  boxShadow: "0 8px 32px rgba(0, 0, 0, 0.2)", // Glass-like shadow
-                  p: 1,
-                  width: 170, // Adjust width to match
-                },
-              }}
-            >
-              <MenuItem
-                sx={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <Typography variant="body2">Share</Typography>
-                <ListItemIcon>
-                  <IoShareOutline size={18} />
-                </ListItemIcon>
-              </MenuItem>
-              <Divider />
-              <MenuItem
-                onClick={DeletePost}
-                sx={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <Typography variant="body2" color="error">
-                  Delete
-                </Typography>
-                <ListItemIcon>
-                  <AiOutlineDelete size={18} color="#d32f2f" />
-                </ListItemIcon>
-              </MenuItem>
-            </Menu>
-          </>
-        )}
-        {/* The Ask */}
-        <Typography
-          variant="body1"
-          sx={{
-            fontWeight: 500,
-            color: "black",
-            whiteSpace: "pre-wrap",
-            "&.MuiTypography-h6": {
-              lineHeight: 1,
-            },
-            textAlign: /^[\u0600-\u06FF]/.test(post.ask.trim())
-              ? "end"
-              : "start",
-            direction: /^[\u0600-\u06FF]/.test(post.ask.trim()) ? "rtl" : "ltr",
-          }}
+        <Stack
+          direction={"row-reverse"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
         >
-          {post.ask}
-        </Typography>
+          <GlassButton>Show Thread</GlassButton>
+          {/* The Ask */}
+          <Typography
+            variant="body1"
+            sx={{
+              fontWeight: 500,
+              color: "black",
+              whiteSpace: "pre-wrap",
+              "&.MuiTypography-h6": {
+                lineHeight: 1,
+              },
+              textAlign: /^[\u0600-\u06FF]/.test(post.ask.trim())
+                ? "end"
+                : "start",
+              direction: /^[\u0600-\u06FF]/.test(post.ask.trim())
+                ? "rtl"
+                : "ltr",
+            }}
+          >
+            {post.ask}
+          </Typography>
+        </Stack>
         {/* Avatar and User Info */}
         <Box sx={{ display: "flex", alignItems: "center", mt: 2 }}>
           <Avatar
@@ -210,24 +183,76 @@ export const Answer = ({ post, avatar, username, userid, useridPosts }) => {
             justifyContent: "space-between",
           }}
         >
-          {/* Heart Icon Toggle */}
-          <IconButton
-            aria-label="like"
-            sx={{ color: post.likes.includes(userid) ? "red" : "#6A6A6A" }}
-            onClick={LikePost}
-            disabled={userid == null}
-          >
-            {post.likes.includes(userid) ? <FaHeart /> : <FaRegHeart />}
-            <Typography variant="body2" color="black" sx={{ ml: 1 }}>
-              {post.likes.length}
-            </Typography>
-          </IconButton>
-          <ReAsk
-            messagehead={post.answer}
-            senderid={userid}
-            receiverid={useridPosts}
-            postId={post.postId}
-          />
+          <Stack direction="row" spacing={1} alignItems="center">
+            {/* Heart Icon Toggle */}
+            <IconButton
+              aria-label="like"
+              sx={{ color: post.likes.includes(userid) ? "red" : "#6A6A6A" }}
+              onClick={LikePost}
+              disabled={userid == null}
+            >
+              {post.likes.includes(userid) ? <FaHeart /> : <FaRegHeart />}
+              <Typography variant="body2" color="black" sx={{ ml: 1 }}>
+                {post.likes.length}
+              </Typography>
+            </IconButton>
+            <ReAsk
+              messagehead={post.answer}
+              senderid={userid}
+              receiverid={useridPosts}
+              postId={post.postId}
+            />
+          </Stack>
+          {useridPosts == userid && (
+            <>
+              <IconButton
+                aria-label="delete"
+                onClick={handleMenuClick}
+                sx={{
+                  color: "#6A6A6A",
+                }}
+              >
+                <PiDotsThreeOutlineLight />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+                PaperProps={{
+                  sx: {
+                    backgroundColor: "rgba(255, 255, 255, 0.25)", // Semi-transparent background
+                    backdropFilter: "blur(10px)", // Frosted glass effect
+                    borderRadius: 2,
+                    border: "1px solid rgba(255, 255, 255, 0.18)",
+                    boxShadow: "0 8px 32px rgba(0, 0, 0, 0.2)", // Glass-like shadow
+                    p: 1,
+                    width: 170, // Adjust width to match
+                  },
+                }}
+              >
+                <MenuItem
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Share</Typography>
+                  <ListItemIcon>
+                    <IoShareOutline size={18} />
+                  </ListItemIcon>
+                </MenuItem>
+                <Divider />
+                <MenuItem
+                  onClick={DeletePost}
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2" color="error">
+                    Delete
+                  </Typography>
+                  <ListItemIcon>
+                    <AiOutlineDelete size={18} color="#d32f2f" />
+                  </ListItemIcon>
+                </MenuItem>
+              </Menu>
+            </>
+          )}
         </Box>
       </Box>
     </Box>
