4349e695e032760d99fc2556316dd8e338f04c28
BigTunaIsYeiling <ahmedenany9812@gmail.com>
2024-11-05 08:52:07 +0200

    aawa

--- middleware.js
+++ middleware.js
@@ -1,28 +1,32 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
+  console.log("Cookies:", request.cookies.getAll()); // Log all cookies
   const token = request.cookies.get("token");
+
   if (!token) {
     console.log("no token");
     return NextResponse.redirect(new URL("/register", request.url));
   }
 
-  const res = await fetch(new URL("/api/verify-token", request.url), {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ token: token.value }),
-  });
+  // Proceed with token verification if token is present
+  try {
+    const res = await fetch(new URL("/api/verify-token", request.url), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: token.value }),
+    });
+    const { valid } = await res.json();
 
-  const { valid } = await res.json();
-  if (valid) {
-    return NextResponse.next();
-  } else {
+    if (valid) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/register", request.url));
+    }
+  } catch (error) {
+    console.error("Error in middleware:", error);
     return NextResponse.redirect(new URL("/register", request.url));
   }
 }
-
-export const config = {
-  matcher: ["/", "/messages", "/notifications"],
-};
